71b9c2d78044c4be614c35dfcf9f034ec07444b7
Abdelmoneim000 <ab000arabi22@gmail.com>
2024-07-16 05:04:06 +0300

    Update(front-end) -> Updating index.js to set dollar sign again in the terminal after each command

--- public/index.js
+++ public/index.js
@@ -1,10 +1,24 @@
 let Xterm = document.getElementById('terminal');
 
+async function sending(sock, input, terminal) {
+    await new Promise(resolve => {
+        sock.send(input); // Send the command to the server
+
+        // Wait for the response from the server
+        sock.onmessage = (event) => {
+            terminal.write(event.data); // Write the server's response to the terminal
+            terminal.write('$ '); // Write $ to the terminal after the output
+            resolve(); // Resolve the promise once response handling is done
+        };
+    });
+}
+
+
 var term = new Terminal({
     convertEol: true
 });
 term.open(Xterm);
-term.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ');
+term.write('$ ');
 
 const socket = new WebSocket('ws://34.207.190.242:3000');
 
@@ -15,9 +29,9 @@ term.onKey(e => {
     );
 
     if (domEvent.keyCode === 13) { // Enter
-        const input = term.buffer.active.getLine(term.buffer.active.cursorY).translateToString().slice(2); // Extract command text
+        const input = term.buffer.active.getLine(term.buffer.active.cursorY).translateToString().slice(1); // Extract command text
         term.write('\r\n');
-        socket.send(input); // Send command to server
+        sending(socket, input, term); // Send command to server
     } else if (domEvent.keyCode === 8) { // Backspace
         if (term.buffer.active.cursorX > 2) {
             term.write('\b \b');
